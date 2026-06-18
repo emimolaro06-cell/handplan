048400744970e36823df779533e5d724beedb95c
emimolaro06-cell <emimolaro06@gmail.com>
2026-06-18 13:31:00 -0300

    Add files via upload

--- handball-v1/src/pages/MonthlyPlanPage.tsx
+++ handball-v1/src/pages/MonthlyPlanPage.tsx
@@ -179,7 +179,7 @@ export function MonthlyPlanPage() {
                 viewMode === 'cycles' ? 'bg-white shadow-sm text-dj-700' : 'text-gray-500 hover:text-gray-700'
               )}
             >
-              <LayoutGrid size={14}/> Microciclos
+              <LayoutGrid size={14}/> Macrociclo
             </button>
           </div>
 
